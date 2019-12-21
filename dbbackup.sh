e80133d83178c7e786891ad9fac685287bc7f9e6
md-jakir <mdavir814@gmail.com>
2019-12-21 19:52:18 +0600

    Multiple website's databases backup sharing to clients

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#dirpath=/test/backuppath/
-dirpath=/var/www/vhosts/default/htdocs/backup/
-dblist=/~/.backup/databses/name/dbname/
-#dblist=/test/list/database/
-#email=/test/list/emails/
-email=/~/.email/list/accounts/
-log=/jakir/tmp2/error.log
+#multiple databases backup
+#Backing up multiple databases for multiple websites in sing server hosted.
+#And sharing to down for clients and send database to client's email.
+dirpath=/path/to/backup/path/
+dblist=/dbname/path/
+email=/email/list/
+log=/log/tmp2/error.log
         max=39; c=0
  while [ $c -le $max ]; do
         cd $dblist
@@ -19,7 +19,7 @@ log=/jakir/tmp2/error.log
                    if [ -d ${dir_list[$i]} ]; then
                       owner=$(ls -ld ${dir_list[$i]} | awk '{print $3}')
                         if [ $owner1 = $owner ]; then
-                           ((cd ${dir_list[$i]} && mysqldump -uadmin -p`cat /etc/psa/.psa.shadow` --opt ${db[$c]} > ${db[$c]}.sql && tar cjf ${db[$c]}_`date +"%d-%m-%Y"`.tbz2 ${db[$c]}.sql && rm -rf ${db[$c]}.sql 2>$log) &)
+                           ((cd ${dir_list[$i]} && mysqldump -u username -p123 --opt ${db[$c]} > ${db[$c]}.sql && tar cjf ${db[$c]}_`date +"%d-%m-%Y"`.tbz2 ${db[$c]}.sql && rm -rf ${db[$c]}.sql 2>$log) &)
                                 max2=25; m=0
                         while [ $m -le $max2 ]; do
                             cd $email; send=(*)
@@ -28,7 +28,7 @@ log=/jakir/tmp2/error.log
                                         if [ $owner = $owner2 ]; then
                                    cd $dirpath"${dir_list[$i]}"
                                         if [ -e ${db[$c]}_`date +"%d-%m-%Y"`.tbz2 ]; then
-                                                echo  "Dear Sir,You are requested to save your databases and web content from "http://webserver57.link3.net/backup/${dir_list[$i]}" and these documents won't be longer in server more than two months once you received. if you unable to access the location then please provide us your network IP. If have any query you can contact with us through vas@link3.net." | mailx -s "Database & content Backup" ${send[$m]}
+                                                echo  "Dear Sir,You are requested to save your databases and web content from "http://www.example.com/backup/${dir_list[$i]}" and these documents won't be longer in server more than two months once you received. if you unable to access the location then please provide us your network IP. If have any query you can contact with us through abc@example.com." | mailx -s "Database & content Backup" ${send[$m]}
                                     fi
                               fi
                         fi
