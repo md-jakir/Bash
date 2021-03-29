72d27b2e163e835e5781a18ba8d7d40ae8a20b13
Jakir Hossain <md-jakir@users.noreply.github.com>
2021-03-29 16:53:05 +0600

    Update maintaindirdata.sh

--- maintaindirdata.sh
+++ maintaindirdata.sh
@@ -1,27 +1,27 @@
 # This script actually remove backup IPLog data from bunch of directory 
+# Removing last directory in a directory loop 
+# maintaining huge directory data in multiple directory 
 
-
-
-#!/bin/bash
-path=/media/content/nfsen-backup/live.backup
-    cd $path
-    dirc=$(ls |wc -l)
+    #!/bin/bash
+    path=/media/content/nfsen-backup/live.backup
+    dirc=$(ls $path |wc -l)
     finc=`expr $dirc - 1`
-        #echo $finc
     tdir=$(ls $path)
     array=($tdir)
-        c=0
-        #for i in ${array[@]}; do
-         while [ $c -le $finc ]; do
-              #if [ "${array[$c]}" == "${array[$c]}" ]; then
-                 #echo ${array[0]}
-                 childir=$(ls $path/${array[0]})
-                 childarr=($childir)
-                 #echo ${childarr[0]}
-                 enddir=$(ls $path/${array[0]}/${childarr[0]})
-                 endarr=($enddir)
-                 rm -rf $path/${array[0]}/${childarr[0]}/${endarr[0]}
-                 exit
-              #fi
-            c=$[$c+1]
-        done
+    c=0
+    while [ $c -le $finc ]; do
+           year=$(ls $path/${array[$c]})
+           yearc=$(ls $path/${array[$c]} | wc -l)
+           year_arr=($year)
+           m=0; max=`expr $yearc - 1`
+       while [ $m -le $max ]; do
+              lsdir=$(ls $path/${array[$c]}/${year_arr[$m]})
+              lsdir_arr=($lsdir)
+              rm -rf "$path/${array[$c]}/${year_arr[$m]}/${lsdir_arr[0]}"
+              m=$[$m+1]
+       done
+
+           c=$[$c+1]
+    done
+
+
