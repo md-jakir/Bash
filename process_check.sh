0335b04adcf335a06f365aa790f4aec2b414f84f
Jakir Hossain <md-jakir@users.noreply.github.com>
2023-03-26 21:14:48 +0600

    check process is running or not

---
+++ process_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function process_check {
+    if [ -z "$1" ]; then
+        echo "Please provide the process name"
+        exit 1
+    fi
+
+    if pgrep -x "$1" >/dev/null; then
+        echo "process $1 is running"
+    else
+        echo "process $1 is not running"
+    fi
+}
+process_check "$1"
