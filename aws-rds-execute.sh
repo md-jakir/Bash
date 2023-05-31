9bc768f2d68e07a060be673fdbbcdddc932d051f
Jakir Hossain <md-jakir@users.noreply.github.com>
2023-05-31 15:20:48 +0600

    Update aws-rds-execute.sh

--- aws-rds-execute.sh
+++ aws-rds-execute.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir /backup
+BK_PATH=/backup
+DB_USER=******
+DB_PASS=************
 echo "Which you want to do?"
 echo
 echo "1 - DB Backup"
@@ -8,80 +12,78 @@ echo "4 - Table Restore"
 echo "5 - Table Rename"
 echo
 TableBakcup() {
-	PATH=/home/ubuntu
-	DB_HOST=database-1.ciahw21bdnzz.ap-south-1.rds.amazonaws.com
-	DB_USER=*****
-	DB_PASS=*********
-	echo "Do you want to take Table Backup?"
-	echo
-	echo "1 - YES"
-	echo "2 - NO"
-	echo
-	read -p "Please Select from the above options: " option
-	if [ "$option" -eq "1" ]; then
-		read -p "Please provide the DB Name: " DB_NAME
-			if [ "$DB_NAME" == "$DB_NAME" ]; then
-				read -p "Please provide your database table name: " TB_NAME
-					if [ "$TB_NAME" == "$TB_NAME" ]; then
-						/usr/bin/mysqldump -h $DB_HOST --set-gtid-purged=OFF -u $DB_USER -p$DB_PASS $DB_NAME $TB_NAME > $PATH/$TB_NAME.sql
-					fi
-			fi
-	fi
+        echo "Do you want to take Table Backup?"
+        echo
+        echo "1 - YES"
+        echo "2 - NO"
+        echo
+        read -p "Please Select from the above options: " option
+        if [ "$option" -eq "1" ]; then
+            read -p "Please provide the DB endpoint: " DB_HOST
+            if [ "$DB_HOST" == "$DB_HOST" ]; then
+                read -p "Please provide the DB Name: " DB_NAME
+                if [ "$DB_NAME" == "$DB_NAME" ]; then
+                    read -p "Please provide your database table name: " TB_NAME
+                    if [ "$TB_NAME" == "$TB_NAME" ]; then
+                        /usr/bin/mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME $TB_NAME > $BK_PATH/$TB_NAME.sql
+                    fi
+                fi
+        	fi
+		if
 }
 
 TableRename() {
-	DB_HOST=database-1.ciahw21bdnzz.ap-south-1.rds.amazonaws.com
-	DB_USER=*****
-	DB_PASS=********
-	echo "Do you want to rename the Table?"
-	echo
-	echo "1 - YES"
-	echo "2 - NO"
-	echo
-	read -p "Please Select from the above options: " option
-	if [ "$option" -eq "1" ]; then
-		read -p "Please provide the DB Name: " DB_NAME
-			if [ "$DB_NAME" == "$DB_NAME" ]; then
-				read -p "Please provide your database table name: " TB_NAME
-					if [ "$TB_NAME" == "$TB_NAME" ]; then
-						read -p "Please provide the new table name: " NEW_TB_NAME
-						if [ "$NEW_TB_NAME" == "$NEW_TB_NAME" ]; then
-							/usr/bin/mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "RENAME TABLE $TB_NAME TO $NEW_TB_NAME;" $DB_NAME
-						fi
-					fi
-			fi
-	fi
+        echo "Do you want to rename the Table?"
+        echo
+        echo "1 - YES"
+        echo "2 - NO"
+        echo
+        read -p "Please Select from the above options: " option
+        if [ "$option" -eq "1" ]; then
+			read -p "Plaese provide the DB endpoint: " DB_HOST
+			if [ "$DB_HOST" == "DB_HOST" ]; then
+            	read -p "Please provide the DB Name: " DB_NAME
+                if [ "$DB_NAME" == "$DB_NAME" ]; then
+                    read -p "Please provide your database table name: " TB_NAME
+                    if [ "$TB_NAME" == "$TB_NAME" ]; then
+                        read -p "Please provide the new table name: " NEW_TB_NAME
+                        if [ "$NEW_TB_NAME" == "$NEW_TB_NAME" ]; then
+                            /usr/bin/mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "RENAME TABLE $TB_NAME TO $NEW_TB_NAME;" $DB_NAME
+                        fi
+					if
+				if
+			if  
+        fi
 }
 
 TableRestore() {
-	PATH=/home/ubuntu
-	DB_HOST=database-1.ciahw21bdnzz.ap-south-1.rds.amazonaws.com
-	DB_USER=*****
-	DB_PASS=***********
-	echo "Do you want to rename the Table?"
-	echo
-	echo "1 - YES"
-	echo "2 - NO"
-	echo
-	read -p "Please Select from the above options: " option
-	if [ "$option" -eq "1" ]; then
-		read -p "Please provide the DB Name: " DB_NAME
-			if [ "$DB_NAME" == "$DB_NAME" ]; then
-				read -p "Please provide your database table name: " TB_NAME
-					if [ "$TB_NAME" == "$TB_NAME" ]; then
-							/usr/bin/mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME < $PATH/$TB_NAME.sql
-					fi
-			fi
-	fi
+        echo "Do you want to rename the Table?"
+        echo
+        echo "1 - YES"
+        echo "2 - NO"
+        echo
+        read -p "Please Select from the above options: " option
+        if [ "$option" -eq "1" ]; then
+            read -p "Please provide the DB endpoint: " DB_HOST
+            if ["$DB_HOST" == "$DB_HOST" ]; then
+            	read -p "Please provide the DB Name: " DB_NAME
+                if [ "$DB_NAME" == "$DB_NAME" ]; then
+                    read -p "Please provide your database table name: " TB_NAME
+                    if [ "$TB_NAME" == "$TB_NAME" ]; then
+                        /usr/bin/mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME < $BK_PATH/$TB_NAME.sql
+                    fi
+                fi
+			if
+        fi
 }
 
 read -p "Please choose from the above options: " option
 if [ "$option" -eq "2" ]; then
-		TableBakcup
-	elif [ "$option" -eq "5" ]; then
-		TableRename
-	elif [ "$option" -eq "4" ]; then
-		TableRestore
-	else
-		"Please enter correct option"
+                TableBakcup
+        elif [ "$option" -eq "5" ]; then
+                TableRename
+        elif [ "$option" -eq "4" ]; then
+                TableRestore
+        else
+                "Please enter correct option"
 fi
